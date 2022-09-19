f96f9fd5d5d8ee449fb7076b0557b091ba8da809
slavikok <slavikokilka@gmail.com>
2022-09-19 18:03:22 +0300

    lab1

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String message = new String();
+        message = "Okilka Yaroslav, KI-34";
+        System.out.println(message);
+    }
+}
